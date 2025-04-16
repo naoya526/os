caa7331bce53a992c3693d88916f64a0b51435cf
naoya526 <naoya.kumakura2003@gmail.com>
2025-04-16 13:43:13 +0200

    add clang pass in run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,12 @@ set -xue
 # QEMUのファイルパス
 QEMU=qemu-system-riscv32
 
+# clangのパス
+CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
 # QEMUを起動
 $QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
 #$QEMU -machine virt -bios default -nographic -serial stdio --no-reboot
+
